f2476d5ca917ccc1e582eb92cf6892f7cf65f09a
TheDerickYoung <derickstech@gmail.com>
2023-06-15 14:05:57 -0700

    Initial commit with directory page updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Directory from "./components/directory/directory.component";
 
-function App() {
+const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "TOPS",
+      imageUrl: "https://i.ibb.co/SP08P4c/IMG-9317.jpg",
+    },
+    {
+      id: 2,
+      title: "BOTTOMS",
+      imageUrl: "https://i.ibb.co/DLZH44t/IMG-9318.jpg",
+    },
+    {
+      id: 3,
+      title: "ACCESSORIES",
+      imageUrl: "https://i.ibb.co/Pt8C75Z/IMG-9315.jpg",
+    },
+    {
+      id: 4,
+      title: "MENS",
+      imageUrl: "https://i.ibb.co/rc8Pj1G/IMG-90-A76-A87-C1-A0-1.jpg",
+    },
+    {
+      id: 5,
+      title: "WOMENS",
+      imageUrl: "https://i.ibb.co/2cPL8Kd/IMG-C287949533-A3-1.jpg",
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Directory categories={categories} />
+    </>
   );
-}
+};
 
 export default App;
